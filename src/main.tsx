11e9275ed9c262d1318702deb069aeba85cc529d
pro <irakizasabin12@gmail.com>
2025-02-16 17:40:01 +0200

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import "./styles/Navbar.css";
+import "./styles/navbar.css";
 import "./styles/Footer.css";
 import "./styles/home.css";
 
